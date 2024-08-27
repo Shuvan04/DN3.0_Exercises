8863de37d7c2012893f8458d9fb8200378cdd141
Shuvan <132642837+Shuvan04@users.noreply.github.com>
2024-08-27 18:36:16 +0530

    Delete Exercise 7 - Introduction to Data Transfer Objects (DTOs)/BookstoreAPI directory

--- Exercise 7 - Introduction to Data Transfer Objects (DTOs)/BookstoreAPI/src/main/java/com/example/BookstoreAPI/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.BookstoreAPI.repository;
-
-import com.example.BookstoreAPI.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
